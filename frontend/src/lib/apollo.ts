7c172089500980567fa536c2937482ed552b9509
conao3 <conao3@gmail.com>
2026-05-16 00:15:29 +0900

    feat(frontend): integrate Apollo Client and provide it in root layout

---
+++ frontend/src/lib/apollo.ts
@@ -0,0 +1,8 @@
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+
+export const apolloClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+  }),
+  cache: new InMemoryCache(),
+});
